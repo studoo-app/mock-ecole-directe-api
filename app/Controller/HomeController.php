d7d1dcf0190668b76d4c1a71d97340314a14fdf5
Benoit Foujols <benoit@foujols.com>
2023-10-06 12:33:17 +0200

    #7 add Edu-Frame

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Controller;
+
+use Studoo\EduFramework\Core\Controller\ControllerInterface;
+use Studoo\EduFramework\Core\Controller\Request;
+use Studoo\EduFramework\Core\View\TwigCore;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class HomeController implements ControllerInterface
+{
+    /**
+     * @param Request $request Requête HTTP
+     * @return string|null
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function execute(Request $request): string|null
+    {
+        return TwigCore::getEnvironment()->render('home/home.html.twig',
+            [
+                'titre'   => 'Hello World !',
+                'requete' => $request
+            ]
+        );
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+// Démarrage de la session PHP pour la gestion des variables de session
+use Studoo\EduFramework\Core\ConfigCore;
+use Studoo\EduFramework\Core\LoadCouchCore;
+
+session_start();
+
+// Masquer les deprecations de PHP 8.1
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+if (version_compare(PHP_VERSION, '8.0', '<') === false) {
+    // Autoloader => chargement automatique des classes depuis le dossier vendor/
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    // Chargement des configurations de l'application
+    (new ConfigCore(
+        [
+            'base_path'         => __DIR__ . '/../',
+            'twig_path'         => __DIR__ . '/../app/Template',
+            'route_config_path'  => __DIR__ . '/../app/config/'
+        ]
+    )
+    );
+
+    // Chargement des classes utilisées par l'application
+    (new LoadCouchCore())->run();
+} else {
+    echo "Cet app nécessite au moins PHP8.0. "
+        . PHP_VERSION .
+        " est actuellement installé. Veuillez mettre à jour votre version de PHP.\n";
+}
